8806c2d3bc12710271a2c04ac9d790e5e642c562
Mirgo25 <mirgo2513@gmail.com>
2024-10-18 11:13:06 +0300

    initialize connecting to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { getSequelizeConfig } from './config/sequelize.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getSequelizeConfig,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/sequelize.config.ts
@@ -0,0 +1,13 @@
+import { ConfigService } from '@nestjs/config';
+import { SequelizeModuleOptions } from '@nestjs/sequelize';
+
+export const getSequelizeConfig = (configService: ConfigService): SequelizeModuleOptions => ({
+  dialect: 'mysql',
+  host: configService.get('DB_HOST'),
+  port: +configService.get('DB_PORT'),
+  username: configService.get('DB_USER'),
+  password: configService.get('DB_PASSWORD'),
+  database: configService.get('DATABASE'),
+  autoLoadModels: true,
+  synchronize: false,
+});
